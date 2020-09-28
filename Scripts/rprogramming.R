8801de5b4108a9909335574681a4cedbc3f3b2a4
tostipolitics <jstrujillo.andes@gmail.com>
2020-09-27 20:02:42 -0500

    Tipos de datos y atributos

--- Scripts/rprogramming.R
+++ Scripts/rprogramming.R
@@ -1,2 +1,35 @@
 # Ver cuáles variables tengo
 source()
+
+# Assignment operator 
+x<-1:20
+msg<-"hello"
+
+#Evaluar función
+print(x)
+
+# []esto muestra el número del vector
+
+#Crear secuencias de números enteros
+1:10
+
+#Vectores
+vector()#"tipo de objeto",largo
+
+#Enteros
+#L sufijo
+1L
+
+#Número infinitos - Inf
+1/0 
+1/Inf
+
+#No es un número - NaN
+0/0
+
+#Atributos
+attributes()
+
+
+
+

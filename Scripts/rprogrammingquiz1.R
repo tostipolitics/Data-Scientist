64f3656636fa2cec8006a49e51ea6ccdd4247bc9
tostipolitics <jstrujillo.andes@gmail.com>
2020-09-29 15:23:04 -0500

    Quiz 1

---
+++ Scripts/rprogrammingquiz1.R
@@ -0,0 +1,49 @@
+#Quiz 1
+
+#Convertir base de datos
+quiz1<-hw1_data
+
+#Ver nombres de las columnas
+colnames(quiz1)
+
+#Ver nombres de las variables
+str(quiz1)
+
+#Extraer filas [n,]
+quiz1[1:2,]
+
+#Extraer columnas [,n]
+quiz1[,1]
+
+#Extraer dato [n,m]
+quiz1[1,1]
+
+#contar columnas y filas
+ncol(quiz1);nrow(quiz1)
+
+#Contar valores nulos por columna
+sapply(quiz1,function(x)sum(is.na(x)))
+
+#Eliminar todos los valores nulos (pocos)
+quiz1
+complete.cases(quiz1)#valores NA son F
+which(complete.cases(quiz1))#filas SIN valores faltantes
+which(!complete.cases(quiz1))#filas CON valores faltantes
+naquiz1<-which(!complete.cases(quiz1))
+naquiz1
+quiz1nona<-quiz1[-naquiz1,]
+quiz1nona#forma 1
+
+quiz1nona<-na.omit(quiz1)
+quiz1nona#forma 2
+
+
+#Sacar media de una columna (también aplica para las demás operaciones aritméticas)
+colMeans(quiz1nona[1])#de una columna en específico
+colMeans(quiz1nona)#de todas las columnas
+filter(quiz)
+
+
+
+#Sacar media con un condicional
+

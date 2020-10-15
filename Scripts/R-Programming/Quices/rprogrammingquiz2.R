a652e635204358b424092c5448ca46238a01fda5
tostipolitics <jstrujillo.andes@gmail.com>
2020-10-15 16:23:02 -0500

    R Programming Quiz 2

--- Scripts/R-Programming/Quices/rprogrammingquiz2.R
+++ Scripts/R-Programming/Quices/rprogrammingquiz2.R
@@ -1,32 +1,149 @@
 #Programming Assessment Week 2
 #1.
-pollutantmean<-function(directory,pollutant,id=1:332){
-  path <- paste0(getwd(),"/", directory) #this one is more general
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
   
-  data <- data.frame()
-  for (i in id) {
-    if (i < 10) {
-      dat <- read.csv(paste(path,"/00", as.character(i),".csv", sep = ""), 
-                      as.is = T, 
-                      header = T)
-      data <- rbind(data,dat)
-    }
-    else if (i < 100) {
-      dat <- read.csv(paste(path,"/0", as.character(i),".csv", sep = ""), 
-                      as.is = T, 
-                      header = T)
-      data <- rbind(data,dat)
-    }
-    else {
-      dat <- read.csv(paste(path,"/", as.character(i),".csv", sep = ""), 
-                      as.is = T, 
-                      header = T)
-      data <- rbind(data,dat)
-    }
-    
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of  the pollutant for which we will calcultate the
+  ## mean; either "sulfate" or "nitrate"
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
+  ## NOTE: Do not round the result
+  means <- c()
+  
+  for(monitor in id){
+    path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+    monitor_data <- read.csv(path)
+    interested_data <- monitor_data[pollutant]
+    means <- c(means, interested_data[!is.na(interested_data)])
   }
-  return(mean(data[,pollutant], na.rm = T))
+  
+  mean(means)
 }
+
+#-------------------------------------------------------------------------------
+
+#2
+complete <- function(directory, id = 1:332){
+  ## 'director' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the from:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  results <- data.frame(id=numeric(0), nobs=numeric(0))
+  for(monitor in id){
+    path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+    monitor_data <- read.csv(path)
+    interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+    interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+    nobs <- nrow(interested_data)
+    results <- rbind(results, data.frame(id=monitor, nobs=nobs))
+  }
+  results
 }
-getwd()
-dir
+
+#-------------------------------------------------------------------------------
+
+#3
+corr <- function(directory, threshold = 0){
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the 
+  ## number of completely observed observations (on all
+  ## variables) requi?red to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  ## NOTE: Do not round the result!
+  cor_results <- numeric(0)
+  
+  complete_cases <- complete(directory)
+  complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+  #print(complete_cases["id"])
+  #print(unlist(complete_cases["id"]))
+  #print(complete_cases$id)
+  
+  if(nrow(complete_cases)>0){
+    for(monitor in complete_cases$id){
+      path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+      #print(path)
+      monitor_data <- read.csv(path)
+      #print(monitor_data)
+      interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+      interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+      sulfate_data <- interested_data["sulfate"]
+      nitrate_data <- interested_data["nitrate"]
+      cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+    }
+  }
+  cor_results
+}
+
+
+#-------------------------------------------------------------------------------
+
+#Ejerciciso Quiz
+#1. 
+pollutantmean("specdata", "sulfate", 1:10)
+
+#2.
+pollutantmean("specdata", "nitrate", 70:72)
+
+#3. 
+pollutantmean("specdata", "sulfate", 34)
+
+#4.
+pollutantmean("specdata", "nitrate")
+
+#5.
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+
+#6.
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+#7. 
+RNGversion("3.5.1")  
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+#8
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+#9.
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+#10.
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))

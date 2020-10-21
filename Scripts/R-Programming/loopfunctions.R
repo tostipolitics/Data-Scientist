4789384ba24412f0c199c8cfde17293e36f182ab
tostipolitics <jstrujillo.andes@gmail.com>
2020-10-21 17:19:21 -0500

    LOOP FUNCTIONS: mapply and tapply

--- Scripts/R-Programming/loopfunctions.R
+++ Scripts/R-Programming/loopfunctions.R
@@ -69,12 +69,41 @@ rowMeans(a,dims = 2)#collapse third dimension
 #-------------------------------------------------------------------------------
 
 #mapply
-#versión multivariada de lapply
+#versión multivariada (múltiples listas) de lapply, corre la función en paralelo sobre un conjunto de argumentos
+str(mapply)
+#FUN es una función a aplicar
+#... argumentos
+#MoreArgs es una lista de argumentos a FUN
+#SIMPLIFY indica si el resultado debe ser simplificado
+list(rep(1,4),rep(2,3),rep(3,2),rep(4,1))
+mapply(rep, 1:4,4:1)#simplificar
+
+noise<-function(n,mean,sd){
+  rnorm(n,mean,sd)
+}
+noise(5,1,2)
+noise(1:5,1:5,2)
+mapply(noise, 1:5,1:5,2)
+
+list(noise(1,1,2),noise(2,2,2),noise(3,3,2),noise(4,4,2),noise(5,5,2))
 
 #-------------------------------------------------------------------------------
 
 #tapply
 #aplica una función sobre los subconjuntos de un vector
+str(tapply)
+#X es un vector
+#INDEX es un factor o lista de factores
+#FUN is la función a aplicar}
+#... argumentos
+#simplify ¿debo simplificar el resultado?
+x<-c(rnorm(10),runif(10),rnorm(10,1))
+x
+f<-gl(3,10)#3 niveles repetidos 10 veces
+f
+tapply(x, f, mean)#vector
+tapply(x, f, mean,simplify = F)#lista
+tapply(x, f, range)# min y max
 
 #-------------------------------------------------------------------------------
 #split

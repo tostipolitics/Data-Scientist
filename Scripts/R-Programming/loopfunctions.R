aa2a685ed48fd81a26e3ec92bb7ae58c9aa9510f
tostipolitics <jstrujillo.andes@gmail.com>
2020-10-23 12:44:58 -0500

    LOOP FUNCTIONS: split

--- Scripts/R-Programming/loopfunctions.R
+++ Scripts/R-Programming/loopfunctions.R
@@ -107,4 +107,32 @@ tapply(x, f, range)# min y max
 
 #-------------------------------------------------------------------------------
 #split
-#función auxiliar en conjunto con lapply
+#toma un vector u otros objetos y los divide en grupos determinado por factores
+#función auxiliar en conjunto con lapply, sapply
+str(split)
+#X es el vector o lista del data frame
+#f es un factor o lista de factores 
+#drop indica si niveles de factores 
+x<-c(rnorm(10),runif(10),rnorm(10,1))
+f<-gl(3,10)
+split(x,f)#siempre devuelve una lista
+
+lapply(split(x,f),mean)
+
+#Hallar la media de la T por mes
+library(datasets)
+head(airquality)
+s<-split(airquality,airquality$Month)
+lapply(s,function(x) colMeans(x[,c("Ozone","Solar.R","Wind")]))
+sapply(s,function(x) colMeans(x[,c("Ozone","Solar.R","Wind")],na.rm = T))
+
+
+#Split en + de un nivel
+x<-rnorm(10)
+f1<-gl(2,5)
+f2<-gl(5,2)
+f1
+f2
+interaction(f1,f2)
+str(split(x,list(f1,f2)))
+str(split(x,list(f1,f2),drop = T))

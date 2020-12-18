0770c69f3ebc55b12e8f3b59da5e89e5e51883ff
tostipolitics <jstrujillo.andes@gmail.com>
2020-12-18 12:04:53 -0500

    Working directory, data types, objects and subsetting

---
+++ Scripts/R-Programming/working-directory-data-types-objects-and-subsetting.R
@@ -0,0 +1,9 @@
+#WORKING DIRECTORY, DATA TYPES, OBJECTS AND SUBSETTING
+
+#-------------------------------------------------------------------------------
+#Working directory
+getwd()
+dir()
+ls()
+read.csv("mydata.csv")
+#Ctrl + a : seleccionar todo

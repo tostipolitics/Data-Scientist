80af2ea26d8bf1cfe60a7ad7d9b3c59c61b08d62
tostipolitics <jstrujillo.andes@gmail.com>
2021-02-05 11:58:48 -0500

    # INTERFACES TO THE OUTSIDE WORLD

---
+++ Scripts/R-Programming/interfaces to the outside world.R
@@ -0,0 +1,38 @@
+# INTERFACES TO THE OUTSIDE WORLD
+
+#-------------------------------------------------------------------------------
+
+## file()
+str(file)
+
+  ## description - name of the file
+
+  ## open ()
+    ### r - read only
+    ### w - writing and initializing a new file
+    ### a - appending
+    ### "rb", "wb", "ab" - reading, writing, or appending in binary mode
+
+con <- file("x", "r")
+data <- read.csv(con)
+close(con)
+### ==
+data <- read.csv("x")
+
+#-------------------------------------------------------------------------------
+
+  ## gzfile() - gzip - reading lines of a text file
+con <- gzfile("archivo")
+x <- readLines(con, 10)
+x
+
+#-------------------------------------------------------------------------------
+
+  ## url() - reading lines in webpages
+con <- url("https://www.instagram.com/")
+x <- readLines(con)
+head(x)
+
+#-------------------------------------------------------------------------------
+
+bz()     ### bzip2

091a56ca29cf11726a7f395362e4ba459a7a8a16
tostipolitics <jstrujillo.andes@gmail.com>
2021-02-11 10:29:43 -0500

    SUBSETTING R OBJECTS

--- Scripts/R-Programming/subsetting.R
+++ Scripts/R-Programming/subsetting.R
@@ -13,6 +13,18 @@ x[u]
 
 #-------------------------------------------------------------------------------
 
+## Matrices
+x <- matrix(1:6, 2, 3) # r x c
+x
+x[1, 2]
+x[2, 1]
+x[1, ]
+x[, 2] # devuelve un vector de largo 1
+x[1, 2, drop = F] # devolver matriz o preservar dimensiones
+x[1, , drop = F]
+
+#-------------------------------------------------------------------------------
+
 ## [[]] - extraer un elemento de una lista o data frame, de cualquier clase
 
 ## $ - extraer elementos de una lista o data frame por el nombre
